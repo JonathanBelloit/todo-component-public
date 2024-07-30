dd5ea4449ef2cfd5fc50173e6dfb806e5927c1d8
Jonathan Belloit <Jonathan@topdogwebdev.com>
2024-07-30 12:40:02 -0400

    fixed modal exit animation

--- src/components/todo/TodoModal.tsx
+++ src/components/todo/TodoModal.tsx
@@ -1,5 +1,4 @@
 import { useAppDispatch } from "../../hooks/useAppDispatch";
-// import TodoButton from './TodoButton';
 import { motion, AnimatePresence } from "framer-motion";
 import { Todo } from "./interface";
 import { FaWindowClose } from "react-icons/fa";
@@ -11,7 +10,6 @@ import {
   MenuItem,
   Select,
   SelectChangeEvent,
-  Stack,
   TextField,
   Typography,
 } from "@mui/material";
@@ -38,8 +36,7 @@ const TodoModal = ({
       setNewTitle(todo.title);
       setNewDescription(todo.description);
     }
-  }, [modalOpen]);
-  if (!modalOpen) return <></>;
+  }, [modalOpen, todo.title, todo.description]);
 
   const handleUpdateTodo = () => {
     const updateTimeStamp = new Date().toString();
@@ -88,129 +85,128 @@ const TodoModal = ({
       },
     },
   };
+
   return (
     <AnimatePresence>
-      <div
-        onClick={handleModalClose}
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          overflowX: "hidden",
-          overflowY: "hidden",
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          zIndex: 1,
-        }}
-      >
+      {modalOpen && (
         <motion.div
-          onClick={(e) => e.stopPropagation()}
+          onClick={handleModalClose}
           style={{
-            display: "flex",
-            position: "relative",
-            flexDirection: "column",
-            border: "1px solid black",
-            borderRadius: "5px",
-            maxWidth: "650px",
-            margin: "2rem auto",
-            backgroundColor: "white",
-            padding: "10px",
-            gap: 10,
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            overflowX: "hidden",
+            overflowY: "hidden",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            zIndex: 1,
           }}
-          variants={modalVariants}
           initial="hidden"
           animate="visible"
           exit="exit"
         >
-          <div
+          <motion.div
+            onClick={(e) => e.stopPropagation()}
             style={{
               display: "flex",
-              borderBottom: "1px solid black",
-              justifyContent: "space-between",
-              alignItems: "center",
-              padding: "0.5rem",
+              position: "relative",
+              flexDirection: "column",
+              border: "1px solid black",
+              borderRadius: "5px",
+              maxWidth: "650px",
+              margin: "2rem auto",
+              backgroundColor: "white",
+              padding: "10px",
+              gap: 10,
             }}
+            variants={modalVariants}
           >
-            <h2>
-              {!editMode ? (
-                todo.title
-              ) : (
-                <TextField
-                  placeholder={todo.title}
-                  value={newTitle}
-                  fullWidth
-                  onChange={(e) => setNewTitle(e.target.value)}
-                />
-              )}
-            </h2>
-            <FaWindowClose
-              onClick={handleModalClose}
-              style={{ fontSize: "1.5rem", color: "#E06546" }}
-            />
-          </div>
-          <div>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between'}}>
-              <Box sx={{ gap: 1, display: 'flex', flexGrow: 3 }}>
-                <Typography variant="h5" color="black">
-                  Project:
-                </Typography>
-                <Typography variant="h5" color="black" fontStyle={'italic'}>
-                  {!todo.project ? 'No project assigned' : todo.project}
-                </Typography>
-              </Box>
-              <FormControl sx={{ mb: 5, width: 200 }}>
-                <InputLabel
-                  sx={{ display: "flex", gap: 2, alignItems: "center" }}
-                  variant="standard"
-                  htmlFor="standard-adornment-amount"
-                >
-                  <Typography variant="h4" color="black">
-                    Urgency:
-                  </Typography>
-                  <Select
-                    onChange={handleChange}
-                    value={todo.urgency}
-                  >
-                    <MenuItem value={"urgent"}>Urgent</MenuItem>
-                    <MenuItem value={"current"}>Current</MenuItem>
-                    <MenuItem value={"back log"}>Back log</MenuItem>
-                  </Select>
-                </InputLabel>
-              </FormControl>
-            </Box>
-            <Box sx={{ my: 2, border: '1px solid black', p: 1 }}>
-              {!editMode ? (
-                todo.description
-              ) : (
-                <TextField
-                  multiline
-                  rows={4}
-                  fullWidth
-                  placeholder={todo.description}
-                  value={newDescription}
-                  onChange={(e) => setNewDescription(e.target.value)}
-                />
-              )}
-            </Box>
-          </div>
-          <Box>
-            <Button
-              onClick={
-                editMode ? handleUpdateTodo : () => setEditMode(!editMode)
-              }
+            <div
+              style={{
+                display: "flex",
+                borderBottom: "1px solid black",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "0.5rem",
+              }}
             >
-              {!editMode ? "Edit" : "Save"}
-            </Button>
-          </Box>
+              <h2>
+                {!editMode ? (
+                  todo.title
+                ) : (
+                  <TextField
+                    placeholder={todo.title}
+                    value={newTitle}
+                    fullWidth
+                    onChange={(e) => setNewTitle(e.target.value)}
+                  />
+                )}
+              </h2>
+              <FaWindowClose
+                onClick={handleModalClose}
+                style={{ fontSize: "1.5rem", color: "#E06546" }}
+              />
+            </div>
+            <div>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between'}}>
+                <Box sx={{ gap: 1, display: 'flex', flexGrow: 3 }}>
+                  <Typography variant="h5" color="black">
+                    Project:
+                  </Typography>
+                  <Typography variant="h5" color="black" fontStyle={'italic'}>
+                    {!todo.project ? 'No project assigned' : todo.project}
+                  </Typography>
+                </Box>
+                <FormControl sx={{ mb: 5, width: 200 }}>
+                  <InputLabel
+                    sx={{ display: "flex", gap: 2, alignItems: "center" }}
+                    variant="standard"
+                    htmlFor="standard-adornment-amount"
+                  >
+                    <Typography variant="h4" color="black">
+                      Urgency:
+                    </Typography>
+                    <Select
+                      onChange={handleChange}
+                      value={todo.urgency}
+                    >
+                      <MenuItem value={"urgent"}>Urgent</MenuItem>
+                      <MenuItem value={"current"}>Current</MenuItem>
+                      <MenuItem value={"back log"}>Back log</MenuItem>
+                    </Select>
+                  </InputLabel>
+                </FormControl>
+              </Box>
+              <Box sx={{ my: 2, border: '1px solid black', p: 1 }}>
+                {!editMode ? (
+                  todo.description
+                ) : (
+                  <TextField
+                    multiline
+                    rows={4}
+                    fullWidth
+                    placeholder={todo.description}
+                    value={newDescription}
+                    onChange={(e) => setNewDescription(e.target.value)}
+                  />
+                )}
+              </Box>
+            </div>
+            <Box>
+              <Button
+                onClick={
+                  editMode ? handleUpdateTodo : () => setEditMode(!editMode)
+                }
+              >
+                {!editMode ? "Edit" : "Save"}
+              </Button>
+            </Box>
+          </motion.div>
         </motion.div>
-      </div>
+      )}
     </AnimatePresence>
   );
 };
 
 export default TodoModal;
-
-// 43 out of 75
-// 18 out of 46
-// 61 out of 121
